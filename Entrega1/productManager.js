bf98546cee9c2e8c21ae709cef6efde1de88d413
enzo_marotti <enzomarotti15@gmail.com>
2023-07-08 10:14:47 -0300

    agregado metodo getProductById y validación por id

--- Entrega1/productManager.js
+++ Entrega1/productManager.js
@@ -3,12 +3,30 @@ class ProductManager {
         this.products = []
     }
 
+    static id = 0;
+
+    //Funcion addProduct que agrega un Id nuevo
     addProduct (title, description, price, thumbnail, code, stock) {
-        this.products.push({title, description, price, thumbnail, code, stock})
+        ProductManager.id++
+        this.products.push({title, description, price, thumbnail, code, stock, id:ProductManager.id})
     }
 
     getProduct(){
         return this.products;
+    }
+
+    //Funcion para buscar productos segun su id.
+    find (id) {
+        return this.products.find((producto) => producto.id === id)
+    }
+
+    //Validación if para evaluar si el producto se encuentra segun id.
+    getProductById (id){
+        if(!this.find(id)){
+            console.log ('Not Found')
+        }else {
+            console.log (this.find(id))
+        }
     }
 }
 
@@ -38,4 +56,11 @@ productos.addProduct(
 "abc124",
 25)
 
+
+// ---Testing ---
+
+//Validación si se pushea al array los productos creados.
 console.log (productos.getProduct());
+
+//Validación de si el ID coincide con el del producto.
+productos.getProductById (2);
